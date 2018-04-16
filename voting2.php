3bcb558efc9888f5bcac95100d415ac8dcb9adea
dchalamalasetty <38415350+dchalamalasetty@users.noreply.github.com>
2018-04-16 10:17:58 +0530

    Create voting2.php

---
+++ voting2.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>nominees</title>
+<script>
+
+</script>
+</head>
+<body>
+<form id="myform">
+<h2> candidates for elections</h2>
+<input type="radio" id="n" name="Section_1" value="you have voted for dinesh" >dinesh <br>
+<input type="radio" id="n" name="Section_1" value="you have voted for shamshu"> shamshu<br>
+<input type="radio" id="n" name="Section_1" value="you have voted for gowri"> gowri<br></form>
+</body>
+</html>
